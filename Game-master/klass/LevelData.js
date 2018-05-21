9b5c050553f2c322ceb3ecd12abaee370d5ea694
C0rruptdeX <39259189+C0rruptdeX@users.noreply.github.com>
2018-05-21 11:58:33 +0200

    -

---
+++ Game-master/klass/LevelData.js
@@ -0,0 +1,37 @@
+
+
+function setNewLevel(nr) {
+
+  var levelNummber = nr;
+
+  if(levelNummber == 1) {
+    game.level.walls_info =  [];
+
+  	game.level.floor_info = [];
+
+  	game.level.keys_end_info = [4850, 4730];
+  	game.level.door_end_info = [4535, 5285, 70, 30];
+
+  	game.level.doors_steel_info = [];
+  	game.level.keys_steel_info = [];
+  	game.level.button_info = [];
+
+  	game.level.activator_info = [];
+
+  	game.level.speer_boxes_info = [];
+  	game.level.speer_umleit_info = [];
+  	game.level.boxes_info = [];
+
+  	game.level.telebs_info = [];
+  	game.level.teleps_info = [];
+
+  	game.level.stoneT_info = [];
+  }
+
+
+
+
+
+
+
+}
